b64875a5c1b5e12ba120e0898bdff9cca1616c0c
squashgray <squashgray@gmail.com>
2019-11-18 15:29:51 -0800

    register

--- testify-fe/src/App.js
+++ testify-fe/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Login from "./components/LoginPage";
 import "./App.css";
+import Register from "./components/Resgister";
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Register />
     </div>
   );
 }
