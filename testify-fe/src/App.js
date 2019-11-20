ca23cc440e34f25f4306b27482461afb14b5b47a
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-19 22:53:16 -0500

    fixing merges from horrible mistake with prior merge from ghpages

---
+++ testify-fe/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./components/LoginPage";
+import "./App.css";
+import Register from "./components/Register";
+import TestList from "./components/TestList";
+
+function App() {
+  return (
+    <div className="App">
+      <TestList />
+    </div>
+  );
+}
+
+export default App;
---
+++ testify-fe/src/components/LoginPage.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import api from "../utils/api";
+
+export default function Login() {
+  const [error, setError] = useState();
+  const [state, setState] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    api()
+      .post("/login", state)
+      .then(res => {
+        console.log("login data", res);
+        localStorage.setItem("token", res.data.token);
+        // props.history.push('/test')
+      })
+      .catch(err => {
+        console.log(err);
+        setError("Unable to authenticate user.");
+      });
+  };
+
+  return (
+    <div>
+      <section>
+        <h2>Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            //   value={data}
+            onChange={handleChange}
+            placeholder="Username"
+          />
+
+          <input
+            type="password"
+            name="password"
+            //   value={data}
+            onChange={handleChange}
+            placeholder="Password"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </section>
+    </div>
+  );
+}
