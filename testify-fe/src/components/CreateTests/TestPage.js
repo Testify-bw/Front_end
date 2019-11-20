8241b9c22f920fef0451a8dd41506794423683ad
squashgray <squashgray@gmail.com>
2019-11-20 09:40:58 -0800

    starting route

--- testify-fe/src/components/CreateTests/TestPage.js
+++ testify-fe/src/components/CreateTests/TestPage.js
@@ -1,6 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function TestPage() {
+  return (
+    <div>
+      <form>
+        <label>
+          Question:
+          <textarea type="text" placeholder="Input your question here" />
+          <label>
+            Answers
+            <input
+              type="text"
+              name="answerA"
+              placeholder="input answer for A"
+            />
+            Answers
+            <input
+              type="text"
+              name="answerB"
+              placeholder="input answer for B"
+            />
+            Answers
+            <input
+              type="text"
+              name="answerC"
+              placeholder="input answer for C"
+            />
+            Answers
+            <input
+              type="text"
+              name="answerD"
+              placeholder="input answer for D"
+            />
+          </label>
+        </label>
+      </form>
+    </div>
+  );
+}
+
 //create button from TestList will take you to this page/compoenet
 
-//where a form of input for test title will be created 
+//where a form of input for test title will be created
 
 //i want to load questions from forms here undert the created test name
 
