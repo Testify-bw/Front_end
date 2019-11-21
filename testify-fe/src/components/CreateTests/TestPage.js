612f54c4c2ca5ddc85b8a14e9fc77810cdd44df0
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-21 14:45:18 -0500

    .put .delete working in testpage

--- testify-fe/src/components/CreateTests/TestPage.js
+++ testify-fe/src/components/CreateTests/TestPage.js
@@ -4,14 +4,57 @@ import FibForm from "./FibForm";
 import { getPost } from '../../actions/index';
 
 import { useDispatch, useSelector } from 'react-redux';
+import api from '../../utils/api';
 
 
 export default function TestPage() {
+  const [test, setTest] = useState({
+    name: '',
+  });
+
+
+  const handleChange = e => {
+    setTest({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    api()
+      .put(`/users/test/1`, test)
+      .then(res => {
+        console.log("put", res);
+
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
 
 
   return (
     <div className="testpage">
       <h2>Hello World</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='name'
+          value={test.name}
+          onChange={handleChange}
+        />
+        <button onClick={(e) => {
+          api()
+            .delete(`/users/test/2`)
+            .then(res => {
+              console.log("delete", res);
+            })
+            .catch(err => {
+              console.log(err);
+            })
+
+        }}>DELETE!!!</button>
+      </form>
     </div>
   );
 }
